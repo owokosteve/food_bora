8d439257b13fafa642c62e11037539e899d16a06
AggaiS <stephanieaggai@gmail.com>
2025-09-09 15:47:57 +0300

    Cart Class

---
+++ Models/Carts.cs
@@ -0,0 +1,22 @@
+namespace foodbora
+{
+    public class Cart
+    {
+        public string ItemId { get; set; }
+        private static int s_itemId = 101;
+        public string OrderId { get; set; }
+        public string FoodId { get; set; }
+        public decimal OrderPrice { get; set; }
+        public int OrderQuantity { get; set; }
+
+        public Cart(string order, string food, decimal price, int quantity)
+        {
+            ItemId = $"ITID{s_itemId++}";
+            OrderId = order;
+            FoodId = food;
+            OrderPrice = price;
+            OrderQuantity = quantity;
+        }
+
+    }
+}
