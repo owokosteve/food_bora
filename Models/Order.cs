a372109520f71a80e35acdc4b39ff66aa9998735
OMONDI-RONNY <129757577+OMONDI-RONNY@users.noreply.github.com>
2025-09-09 15:44:25 +0300

    created order class

---
+++ Models/Order.cs
@@ -0,0 +1,27 @@
+namespace foodbora
+{
+    enum OrderStatus
+    {
+        Initiated = 0,
+        Ordered = 1,
+        Cancelled = 2
+    }
+    class Order
+    {
+        public string OrderId { get; set; }
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        private static int counter = 1001;
+
+        public Order(string userId, DateTime orderDate, decimal totalPrice, OrderStatus orderStatus)
+        {
+            OrderId = $"OID{counter++}";
+            UserId = userId;
+            OrderDate = orderDate;
+            TotalPrice = totalPrice;
+            OrderStatus = orderStatus;
+        }
+    }
+}
