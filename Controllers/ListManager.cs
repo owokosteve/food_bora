d46a999b01a02f6e1b14f6516b14620dbb3ac083
Stephen Owoko <40646976+owokosteve@users.noreply.github.com>
2025-09-15 12:14:04 +0000

    Added access modifiers

--- Controllers/ListManager.cs
+++ Controllers/ListManager.cs
@@ -2,10 +2,10 @@ namespace foodbora;
 
 public class ListManager
 {
-    List<User> users = new();
-    List<Food> foods = new();
-    List<Order> orders = new();
-    List<Cart> carts = new();
+    public static List<User> users = new();
+    public static List<Food> foods = new();
+    public static List<Order> orders = new();
+    public static List<Cart> carts = new();
 
     public void AddUser(User user) => users.Add(user);
     public void AddFood(Food food) => foods.Add(food);
